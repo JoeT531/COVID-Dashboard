b93f78678d6ea14e84eb6de33d483db8e722e2b8
JoeT531 <57190859+JoeT531@users.noreply.github.com>
2020-04-29 06:29:08 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -32,8 +32,12 @@ ui <- navbarPage("COVID19",
                           
                             column(9,
                                    fluidRow(
-                                             column(12,div(style = "height:225px",
+                                             column(12,div(style = "height:240px",
+                                                           p("county stats"),
                                                            plotlyOutput("county_graph")))),
+                                   fluidRow(column(12,div(style = "height:25px",  wellPanel(style = "padding: .5px;")
+                                                                                            
+                                                                                            ))),
                                    fluidRow( column(12,
                                                     column(8,leafletOutput("tract_map")),
                                                     column(4,plotlyOutput("tract_graph")))),
@@ -41,13 +45,13 @@ ui <- navbarPage("COVID19",
                                 #     fluidRow(column(12,offset=4,htmlOutput("Click_bounds")))
                                    # verbatimTextOutput("click_table")
                           )),
-                          fluidRow(column(12,offset = 4, 
-                                          tags$img(src = 'NewYorkTimes_Logo.png',width = '200px'),
+                          fluidRow(column(12,div(style = "height:1px",
+                                         # tags$img(src = 'NewYorkTimes_Logo.png',width = '200px'),
                                           tags$img(src = 'tbd_logo.png', width = "200px", 
                                                                  align = "center"),
-                                          tags$img(src = 'CDC-logo.png',width = '70px',height = '50px')
+                                         # tags$img(src = 'CDC-logo.png',width = '70px',height = '50px')
                                                                                               
-                          ))),
+                          )))),
                  tabPanel("Component 2"),
                  tabPanel("Component 3")
 )
